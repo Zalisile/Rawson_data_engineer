a9d18dc3f54e98c37648d179e4b9ee4a2f0bbf64
Zalisile <41106299+Zalisile@users.noreply.github.com>
2023-07-15 11:21:18 +0000

    Adding data to the table people's table

--- images/add_data_app/app.py
+++ images/add_data_app/app.py
@@ -8,13 +8,13 @@ deprecations.SILENCE_UBER_WARNING = True
 
 
 # Delay for a few seconds before connecting to the database
-print("Waiting for 80 seconds.")
-time.sleep(80)
+print("Waiting for 120 seconds.")
+time.sleep(120)
 print("Wait is over.")
 
 
 # Connect to the database
-msqldb_uri = 'mysql://codetest:swordfish@database:3307/codetest'
+msqldb_uri = 'mysql://codetest:swordfish@database/codetest'
 engine = sqlalchemy.create_engine(msqldb_uri)
 
 # Delay for a few seconds before connecting to the database
@@ -35,8 +35,8 @@ with open('./data/people.csv') as csv_file:
     for row in reader:
         rows.append({'given_name': row[0], 'family_name': row[1], 'date_of_birth': row[2], 'place_of_birth': row[3] })
     # Delay for a few seconds before connecting to the database
-    print("Waiting for 5 seconds.")
-    time.sleep(5)
+    print("Waiting for 15 seconds.")
+    time.sleep(15)
     print("Wait is over.")    
 
     # Insert multiple rows at once
