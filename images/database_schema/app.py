c13dfde7a378662b76908caf713011467bc6e0a6
Zalisile <41106299+Zalisile@users.noreply.github.com>
2023-07-15 09:42:27 +0000

    Fixing the code structure

---
+++ images/database_schema/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import time
+import sqlalchemy
+from sqlalchemy import text
+from sqlalchemy.util import deprecations
+deprecations.SILENCE_UBER_WARNING = True
+
+print("Waiting for 5 seconds.")
+time.sleep(5)
+print("Wait is over.")
+
+# Connect to the database
+msqldb_uri = 'mysql://codetest:swordfish@database:3306/codetest'
+engine = sqlalchemy.create_engine(msqldb_uri)
+
+ 
+# Execute schema.sql
+with engine.connect() as con:
+    with open('./schema.sql') as file:
+        query = text(file.read())
+        con.execute(query)
+
+ 
+print("Waiting for 5 seconds.")
+time.sleep(5)
+print("Wait is over.")
+
+ 
+# Fetch tables and print them
+inspector = sqlalchemy.inspect(engine.connect())
+table_names = inspector.get_table_names()
+for table_name in table_names:
+    print(table_name)
