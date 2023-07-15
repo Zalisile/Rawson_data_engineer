6a656424f75d0efe4b860159e40f9bfa4c447780
Zalisile <41106299+Zalisile@users.noreply.github.com>
2023-07-15 10:36:41 +0000

    Fixing the indetation on the app file

--- images/schema_app/app.py
+++ images/schema_app/app.py
@@ -16,19 +16,3 @@ engine = sqlalchemy.create_engine(msqldb_uri)
 print("Waiting for 5 seconds.")
 time.sleep(5)
 print("Wait is over.")
-
-# Execute schema.sql
-with engine.connect() as con:
-    with open('./schema.sql') as file:
-        query = text(file.read())
-        con.execute(query)
-
-print("Waiting for 5 seconds.")
-time.sleep(5)
-print("Wait is over.")
-
-# Fetch tables and print them
-inspector = sqlalchemy.inspect(engine.connect())
-table_names = inspector.get_table_names()
-for table_name in table_names:
-    print(table_name)
